64a5c7df09bf01079c09187fcba7cee2fb28f5f3
moamlrh <moamll288@gmail.com>
2021-09-16 20:56:57 +0300

    added default value for items prop

--- src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -8,7 +8,7 @@ namespace Basket.API.Entities
     public class ShoppingCart
     {
         public string UserName { get; set; }
-        public List<ShoppingCartItem> Items { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
         public ShoppingCart()
         {
         }
